2dcc41a0b9c14e36920101ed443871d5456f2153
landlab <landlab@gmail.com>
2020-06-26 19:25:23 +0200

    handle general REST API errors to result in adequate HTTP statuses

--- src/test/kotlin/io/maha/checkout/controller/CheckoutControllerTest.kt
+++ src/test/kotlin/io/maha/checkout/controller/CheckoutControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 
 @WebMvcTest(controllers = [CheckoutController::class])
@@ -67,6 +68,17 @@ class CheckoutControllerTest {
             accept = MediaType.APPLICATION_JSON
         }.andExpect {
             status { isInternalServerError }
+        }
+    }
+
+    @Test
+    fun `should return MethodNotAllowed status in case of unsupported HTTP method is used`() {
+        mockMvc.get("/checkout") {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(watchIds)
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isMethodNotAllowed }
         }
     }
 }
