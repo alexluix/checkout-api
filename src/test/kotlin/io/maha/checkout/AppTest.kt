b6d488231ab6c846e1d78fe24628be46de80e952
landlab <landlab@gmail.com>
2020-06-26 16:54:21 +0200

    add spring context smoke test

---
+++ src/test/kotlin/io/maha/checkout/AppTest.kt
@@ -0,0 +1,11 @@
+package io.maha.checkout
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AppTest {
+    @Test
+    fun `should start context`() {
+    }
+}
